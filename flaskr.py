202a4953cf8ff67b47b4e4a191a72093633dc859
Craig Meinschein <cmeinsch@gmail.com>
2014-06-22 16:18:15 -0700

    Implemented GET stations/all REST function.

--- flaskr.py
+++ flaskr.py
@@ -1,12 +1,37 @@
 
 from flask import Flask, request, render_template
 from flask.ext.bootstrap import Bootstrap
-
+import json
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
 
+# Set debug mode.
+debug = False
+
+# REST API function definitions
+
+# Stations
+@app.route('/REST/1.0/stations/all/')
+def all_stations():
+    db = open('data/stations.json','r')
+    stations = json.load(db)
+    return json.dumps(stations, ensure_ascii=True)
+
+@app.route('/REST/1.0/stations/all/<float:lat>/<float:lon>/<float:rad>')
+def all_stations_in_rad(lat, lon, rad):
+    return all_stations()
+
+
+# Riders
+
+# Bikes
+
+# Trips
+
+
+@app.route('/')
 
 @app.route('/index')
 def index():
@@ -29,6 +54,8 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    if debug:
+        app.run(host='127.0.0.1', port=8080, debug=True)
+    else:
+        app.run(host='0.0.0.0', port=8080, debug=False)
 
