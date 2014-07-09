52b4f8b8e3edc276383b716396c73ea1bda21885
Craig Meinschein <cmeinsch@gmail.com>
2014-07-08 19:34:33 -0700

    Reverted port change.

--- bikeshare.py
+++ bikeshare.py
@@ -274,7 +274,7 @@ def internal_server_error(e):
 
 if __name__ == '__main__':
     if debug:
-        app.run(host='127.0.0.1', port=8085, debug=True)
+        app.run(host='127.0.0.1', port=8081, debug=True)
     else:
-        app.run(host='0.0.0.0', port=8085, debug=True)
+        app.run(host='0.0.0.0', port=8081, debug=True)
 
