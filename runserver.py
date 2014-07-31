e3c9e6eb206b1c4af2cb901e5ee8dfd04cea6423
clayton ward <war5@pdx.edu>
2014-07-31 16:55:14 -0700

    added runserver.py with breaks out the run configuration

---
+++ runserver.py
@@ -0,0 +1,8 @@
+from bikeshare import app
+debug = False
+if __name__ == '__main__':
+    if debug:
+        app.run(host='127.0.0.1', port=8089, debug=True)
+    else:
+        app.run(host='0.0.0.0', port=8089, debug=False)
+
