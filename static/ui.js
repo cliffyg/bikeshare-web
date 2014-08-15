b7130e7f2bae224a0dbcbcea7095e36b193c1694
Jonathan Hong <jonathan.hong8@gmail.com>
2014-08-14 22:23:14 -0700

    moved About link outside of logo, resized/centered logo on navbar

--- static/ui.js
+++ static/ui.js
@@ -24,11 +24,11 @@ function update_stats() {
 
 function check_anomalies() {
     $.getJSON(api_url + "/REST/1.0/anomalies", function(data) {
-        if($("anomalies").is(":visible")) {
+        if (!$("anomalies").is(":visible")) {
             $("anomalies").show();
         }
         $.each(data['anomalies'], function(i, anomaly) {
-            console.log(anomaly);
+        
         });
     })
         .fail(function() {
